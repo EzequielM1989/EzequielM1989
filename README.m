56bca8920b499e1fd41b97dfc6b6ad41483b5d41
EzequielM1989 <128399616+EzequielM1989@users.noreply.github.com>
2023-03-20 13:30:57 -0300

    Update README.m

--- README.m
+++ README.m
@@ -1,1 +1,7 @@
-Hi! I am Ezequiel, and I’m 33 years old. I am an electronics engineer with ten years of experience in telecommunications, specifically with radiofrequency circuits and antennas. Now, I am interested in learning about cloud computing.
+Hi! :wave:
+
+I am Ezequiel, and I’m 33 years old. I am an electronics engineer from the UNLP, Argentina, with ten years of experience in telecommunications, specifically with radiofrequency circuits and antennas.
+
+I am currently interested in learning about cloud computing, and I am also looking to collaborate on data science projects.
+
+Outside the virtual world, I like to play football, spending time with friends and enjoy the summer!
